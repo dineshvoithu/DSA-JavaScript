5889ccd75ee535ab60cae9abb9180da36d3c378f
dineshvoithu <dineshkumarvoithu@gmail.com>
2025-09-19 11:56:51 +0530

    java day 2

---
+++ java/day-2/Rotate.java
@@ -0,0 +1,16 @@
+import java.util.Arrays;
+
+public class Rotate {
+    public static void main(String[] args) {
+        int[] nums = { 1, 2, 3, 4, 5, 6, 7 };
+        int k = 3;
+        int n = nums.length;
+        int[] rotated = new int[n];
+
+        for (int i = 0; i < n; i++) {
+            rotated[(i + k) % n] = nums[i];
+        }
+        System.out.println(Arrays.toString(rotated));
+
+    }
+}
---
+++ java/day-2/StringDem.java
@@ -0,0 +1,13 @@
+public class StringDem {
+    public static void main(String[] args) {
+        String s = "Hello Java";
+        System.out.println(s.length());
+        System.out.println(s.charAt(0));
+        System.out.println(s.charAt(9));
+        System.out.println(s.substring(6, 10));
+        System.out.println(s.indexOf("lo"));
+        System.out.println(s.replace("java", "world"));
+
+    }
+
+}
